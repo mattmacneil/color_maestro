1dd87f00e02e2bc352fbc260c169db08fb12e4ed
Matt MacNeil <matt@mattmacneil.com>
2017-06-16 15:27:12 -0400

    Added h1 and h1 animation.

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
     include "head.php";
-    include "nav.php";
 ?>
 
-index file!
+<div class="content-container">
 
-<div class="hi">hi</div>
+	<div class="content-container-inner">
 
+		<span class="title-name">Kenneth Fielding:</span>
+
+		<h1>Colour<br />Maestro.</h1>
+
+	</div><!-- content container inner -->
+
+</div><!-- content container -->
 
 <?php 
 
